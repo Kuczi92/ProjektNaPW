466fd9654512906e7311ce69043461decfecd17f
Norbert Kuczera <norbertkuczera@gmail.com>
2017-01-18 03:21:30 +0100

    Stworzono gui

---
+++ src/pw_imageprocessing/PW_ImageProcessing.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pw_imageprocessing;
+
+
+
+/**
+ *
+ * @author Quchi
+ */
+public class PW_ImageProcessing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        OknoGłówneProgramu  Main =  new OknoGłówneProgramu();
+        
+        
+        
+       // Create main window
+		
+                
+                
+                
+                
+                
+    }
+    
+}
